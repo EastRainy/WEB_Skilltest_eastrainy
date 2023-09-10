77113e05ac95e931c11c92e2dc8f7f01e0bccc74
EastRainy <dlfldhqka@naver.com>
2023-09-11 00:24:46 +0900

    Controller change, transfer crypt impl

--- src/main/java/com/practice/Skilltest/user/controller/UserController.java
+++ src/main/java/com/practice/Skilltest/user/controller/UserController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.http.HttpRequest;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -19,30 +22,31 @@ public class UserController {
 
     @Autowired
     UserServiceImpl userService;
-    @Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    //로그인페이지 요청
     @GetMapping("/login")
     public String login_get(Model model){
         model.addAttribute("user", new UserEntity());
         return "html/user/login";
     }
-
+    //로그인시 에러처리할경우
     @PostMapping("/login")
-    public String login_post(Model model){
+    public String login_post(Model model, HttpServletRequest request){
+        model.addAttribute("message",request.getAttribute("message"));
         model.addAttribute("user", new UserEntity());
         return "html/user/login";
     }
-
+    
+    //회원가입페이지 요청
     @GetMapping("/signup")
     public String signup_get(Model model){
         model.addAttribute("user", new UserEntity());
         return "html/user/signup";
     }
 
+
     @PostMapping("/signup")
     public String signup_post(UserEntity user){
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userService.signupUser(user);
         return "redirect:/login";
     }
