ce2f6c59cbbe58b830d6172e7c38b9f4266b7101
EastRainy <48935971+EastRainy@users.noreply.github.com>
2023-11-16 18:10:50 +0900

    Main page navbar test

--- src/main/java/com/practice/Skilltest/controller/MainController.java
+++ src/main/java/com/practice/Skilltest/controller/MainController.java
@@ -2,13 +2,22 @@ package com.practice.Skilltest.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,5 +35,24 @@ public class MainController {
         return "html/main";
     }
 
+    @GetMapping("/welcome")
+    public String getWelcome(Model model, @AuthenticationPrincipal User user){
+            return "html/welcome";
+    }
+
+
+    @GetMapping("/navbar")
+    @ResponseBody
+    public ResponseEntity<String> getNavbar(){
+        try{
+            Path path = Paths.get("src/main/resources/templates/html/navbar.html");
+            String navbarContent = new String(Files.readAllBytes(path));
+
+            return ResponseEntity.status(HttpStatus.OK).body(navbarContent);
+        } catch(IOException e){
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("File Not Found");
+        }
+    }
 
 }
