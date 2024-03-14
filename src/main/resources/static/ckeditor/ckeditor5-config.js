7c5d9da3835f2bea39f804fd82d4ccd38c03e1a8
EastRainy <48935971+EastRainy@users.noreply.github.com>
2024-03-14 11:28:45 +0900

    #32 import character counter / if exceed limit, button disable

--- src/main/resources/static/ckeditor/ckeditor5-config.js
+++ src/main/resources/static/ckeditor/ckeditor5-config.js
@@ -1,7 +1,20 @@
 
+const maxCharacter = 2000;
+const wordCounter = document.querySelector('.editor-words-counter');
+const sendButton = document.getElementById('send-button');
+
 ClassicEditor
 	.create( document.querySelector( '#editor' ), {
 		// Editor configuration.
+		wordCount:{
+		    onUpdate: stats =>{
+
+		        const limitExceed = stats.characters > maxCharacter;
+
+		        wordCounter.textContent = `${stats.characters} / 2000`;
+		        sendButton.toggleAttribute('disabled', limitExceed);
+		    }
+		}
 	} )
 	.then( editor => {
 		window.editor = editor;
