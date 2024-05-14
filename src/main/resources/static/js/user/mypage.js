88c66f96c5014046933f6c73a8cec4fd082c7017
EastRainy <48935971+EastRainy@users.noreply.github.com>
2024-05-14 16:49:36 +0900

    todo update/unused function removed

--- src/main/resources/static/js/user/mypage.js
+++ src/main/resources/static/js/user/mypage.js
@@ -34,30 +34,3 @@ function initEmailValue(){
     document.getElementById('email2').value = splitEmailValue[1];
 }
 
-
-//나눠진 email값 합성하여 userData email에 넣는 함수
-function emailCombine(){
-
-    let emailValue = '';
-
-    emailValue += document.getElementById("email1").value;
-    emailValue += '@';
-    emailValue += document.getElementById("email2").value;
-
-    document.getElementById('email').value = emailValue;
-
-}
-
-//나눠진 phone값 합성하여 userData phone에 넣는 함수
-function phoneCombine(){
-
-    let phoneValue = '';
-    for(let i = 1 ; i<4; i++){
-        phoneValue += document.getElementById("phone"+i).value;
-        if(i!==3){
-            phoneValue += '-';
-        }
-    }
-    document.getElementById("phone").value = phoneValue;
-}
-
